081cca3e8e1a6228619cf478154db289b92d6e39
popo2381 <popo2381@icloud.com>
2026-04-03 20:59:19 +0900

    feat: Point Entity 및 Repository 구현

---
+++ src/main/java/com/popo2381/coffeeshop/domain/point/entity/Point.java
@@ -0,0 +1,31 @@
+package com.popo2381.coffeeshop.domain.point.entity;
+
+import com.popo2381.coffeeshop.domain.user.entity.User;
+import com.popo2381.coffeeshop.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "points")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Point extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private int balance;
+
+    public Point(User user, int balance) {
+        this.user = user;
+        this.balance = balance;
+    }
+}
