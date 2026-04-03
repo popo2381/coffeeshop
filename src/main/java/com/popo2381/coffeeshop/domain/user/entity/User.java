15aac2c2ccd6d3ae4b99f213d2e5bdd22161298e
popo2381 <popo2381@icloud.com>
2026-04-03 20:57:40 +0900

    feat: User Entity 및 Repository 구현
    - application.yml 설정 추가
      - defer-datasource-initialization: true
      - spring.sql.init.mode: always

---
+++ src/main/java/com/popo2381/coffeeshop/domain/user/entity/User.java
@@ -0,0 +1,22 @@
+package com.popo2381.coffeeshop.domain.user.entity;
+
+import com.popo2381.coffeeshop.global.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
