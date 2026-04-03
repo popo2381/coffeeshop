ed94190e129f5bb1a48f8557325de62f10278135
popo2381 <popo2381@icloud.com>
2026-04-03 20:42:07 +0900

    chore: 초기 메뉴 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into menus (name, price, created_at, updated_at)
+values ('아메리카노', 3000, now(), now());
+
+insert into menus (name, price, created_at, updated_at)
+values ('카페라떼', 4000, now(), now());
+
+insert into menus (name, price, created_at, updated_at)
+values ('카푸치노', 4500, now(), now());
