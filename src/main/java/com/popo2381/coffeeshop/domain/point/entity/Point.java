bd5ec06428a5263dc14cad7acbe5a539205c3d5a
popo2381 <popo2381@icloud.com>
2026-04-05 17:09:24 +0900

    feat: 포인트 차감 도메인 로직 추가
    - Point 엔티티에 잔액 검증 및 포인트 차감 메서드 추가

--- src/main/java/com/popo2381/coffeeshop/domain/point/entity/Point.java
+++ src/main/java/com/popo2381/coffeeshop/domain/point/entity/Point.java
@@ -2,6 +2,8 @@ package com.popo2381.coffeeshop.domain.point.entity;
 
 import com.popo2381.coffeeshop.domain.user.entity.User;
 import com.popo2381.coffeeshop.global.common.entity.BaseEntity;
+import com.popo2381.coffeeshop.global.error.code.PointErrorCode;
+import com.popo2381.coffeeshop.global.error.exception.BusinessException;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -29,11 +31,22 @@ public class Point extends BaseEntity {
         this.balance = balance;
     }
 
+    // 최초 생성 시 포인트 잔액은 0으로 시작
     public static Point create(User user) {
         return new Point(user, 0);
     }
 
+    // 포인트 충전
     public void charge(int amount) {
         this.balance += amount;
+    }
+
+    // 포인트 사용
+    public void use(int amount) {
+        // 잔액보다 큰 금액을 사용하려고 하면 예외 처리
+        if (this.balance < amount) {
+            throw new BusinessException(PointErrorCode.INSUFFICIENT_POINT);
+        }
+        this.balance -= amount;
     }
 }
