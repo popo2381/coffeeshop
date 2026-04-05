bcf491b7a337ee65af1cfe2c57fd4d9e33d9d750
popo2381 <popo2381@icloud.com>
2026-04-05 23:23:43 +0900

    feat: 주문 단건 조회 API 구현

--- src/main/java/com/popo2381/coffeeshop/domain/order/controller/OrderController.java
+++ src/main/java/com/popo2381/coffeeshop/domain/order/controller/OrderController.java
@@ -2,15 +2,13 @@ package com.popo2381.coffeeshop.domain.order.controller;
 
 import com.popo2381.coffeeshop.domain.order.dto.request.OrderCreateRequest;
 import com.popo2381.coffeeshop.domain.order.dto.response.OrderCreateResponse;
+import com.popo2381.coffeeshop.domain.order.dto.response.OrderDetailResponse;
 import com.popo2381.coffeeshop.domain.order.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -19,6 +17,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    // 주문 생성 API
     @PostMapping
     public ResponseEntity<OrderCreateResponse> create(@Valid @RequestBody OrderCreateRequest request) {
 
@@ -30,5 +29,11 @@ public class OrderController {
 
         // 2. 주문 생성 결과 반환
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // 주문 단건 조회 API
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDetailResponse> getOrder(@PathVariable Long orderId) {
+        return ResponseEntity.ok(orderService.getOrder(orderId));
     }
 }
